814ab4a171f999b9daae93e1438552a3b79e1539
srinivas7143 <srinivaskona76@gmail.com>
2019-10-01 16:15:05 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
+sudo docker -v
+var=$(docker -v)
+echo "your docker version is ${var}"
